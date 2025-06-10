515a4374f78a72e784d636df94b8a9bb8efb8abf
Samantha Nguyen <contact@samanthanguyen.me>
2025-06-10 13:38:25 -0500

    feat: improve code outlining
    - support for annotations
    - support for namespace, union, scalar, interface, and enum statements

--- languages/typespec/outline.scm
+++ languages/typespec/outline.scm
@@ -1,6 +1,42 @@
+(namespace_statement
+	(annotation_list)? @annotation
+	"namespace" @context
+	name: (_) @name
+	) @item
+
 (model_statement
+	(annotation_list)? @annotation
 	"model" @context
 	name: (identifier) @name
 	(template_parameters)? @context.extra
 	(model_is_heritage)? @context.extra
+	) @item
+
+(union_statement
+	(annotation_list)? @annotation
+	"union" @context
+	name: (_) @name
+	(template_parameters)? @context.extra
+	) @item
+
+(scalar_statement
+	(annotation_list)? @annotation
+	"scalar" @context
+	name: (_) @name
+	(template_parameters)? @context.extra
+	(scalar_extends)? @context.extra
+	) @item
+
+(interface_statement
+	(annotation_list)? @annotation
+	"interface" @context
+	name: (_) @name
+	(template_parameters)? @context.extra
+	(interface_heritage)? @context.extra
+	) @item
+
+(enum_statement
+	(annotation_list)? @annotation
+	"enum" @context
+	name: (_) @name
 	) @item
